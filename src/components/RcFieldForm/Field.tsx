7e5be969ab10add01e102ea92572402e6970b90e
sheben404 <sheben404@gmail.com>
2023-03-12 23:07:20 +0800

    refactor: refactor the Field component into a functional component.

--- src/components/RcFieldForm/Field.tsx
+++ src/components/RcFieldForm/Field.tsx
@@ -1,4 +1,4 @@
-import React, { ReactElement } from 'react'
+import React, { FC, ReactElement, useContext, useLayoutEffect, useReducer } from 'react'
 import FieldContext from './FieldContext'
 import { FieldEntity } from './FormStore'
 
@@ -11,40 +11,68 @@ export type FieldProps = {
   }>
 }
 
-class Field extends React.Component<FieldProps> implements FieldEntity {
-  static contextType = FieldContext
-  declare context: React.ContextType<typeof FieldContext>
+// class Field extends React.Component<FieldProps> implements FieldEntity {
+//   static contextType = FieldContext
+//   declare context: React.ContextType<typeof FieldContext>
 
-  unregister: () => void = () => {}
+//   unregister: () => void = () => {}
 
-  componentDidMount(): void {
-    this.unregister = this.context.getInternalHooks().registerField(this)
-  }
+//   componentDidMount(): void {
+//     this.unregister = this.context.getInternalHooks().registerField(this)
+//   }
 
-  componentWillUnmount(): void {
-    this.unregister()
-  }
+//   componentWillUnmount(): void {
+//     this.unregister()
+//   }
 
-  onStoreChange = () => {
-    this.forceUpdate()
-  }
+//   onStoreChange = () => {
+//     this.forceUpdate()
+//   }
 
-  getControlled = () => {
-    const { getFieldValue, setFieldsValue } = this.context
-    const { name } = this.props
+//   getControlled = () => {
+//     const { getFieldValue, setFieldsValue } = this.context
+//     const { name } = this.props
+//     return {
+//       value: getFieldValue(name),
+//       onChange: (e: React.ChangeEvent<HTMLInputElement>) => {
+//         const newValue = e.target.value
+//         setFieldsValue({ [name]: newValue })
+//       },
+//     }
+//   }
+//   render() {
+//     const { children } = this.props
+//     const returnElement = React.cloneElement(children, this.getControlled())
+//     return returnElement
+//   }
+// }
+
+const Field: FC<FieldProps> = (props) => {
+  const { children, name } = props
+  const fieldContext = useContext(FieldContext)
+  const [, forceUpdate] = useReducer((state) => state + 1, 1)
+
+  useLayoutEffect(() => {
+    const unregister = fieldContext.getInternalHooks().registerField({
+      props,
+      onStoreChange: () => {
+        forceUpdate()
+      },
+    })
+    return unregister
+  }, [])
+  const getControl = () => {
     return {
-      value: getFieldValue(name),
+      value: fieldContext.getFieldValue(name),
       onChange: (e: React.ChangeEvent<HTMLInputElement>) => {
-        const newValue = e.target.value
-        setFieldsValue({ [name]: newValue })
+        const value = e.target.value
+        fieldContext.setFieldsValue({
+          [name]: value,
+        })
       },
     }
   }
-  render() {
-    const { children } = this.props
-    const returnElement = React.cloneElement(children, this.getControlled())
-    return returnElement
-  }
+  return React.cloneElement(children, getControl())
 }
 
 export default Field
